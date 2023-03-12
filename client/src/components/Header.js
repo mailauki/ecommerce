8be1913a119e1e9b3493adfd35df0e20a334b5af
Julie Evans <juliemevans17@gmail.com>
2023-03-12 15:08:33 -0500

    Update Header styles

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,39 +1,49 @@
 // import '../styles/Header.css';
 import { Link, useLocation } from 'react-router-dom';
-import { Avatar, Box, Button, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, Avatar, Box, Button, IconButton } from '@mui/material';
 import CartButton from './CartButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
     const { pathname } = useLocation()
 
     return (
-        <div className="Header">
-            <Box sx={{ "&:hover": { color: "primary.main" }}}>
-                <Link to="/">
-                    <h3>Header</h3>
-                </Link>
-            </Box>
-            {false ? (
-                <>
-                    <CartButton />
-
-                    <IconButton>
-                        <Avatar 
-                            alt="username"
-                            src=""
-                            sx={{ width: 24, height: 24 }} 
-                        />
-                    </IconButton>
-                </>
-            ) : (
-                <Button 
-                    component={Link} to="/login" 
-                    variant="contained"
-                    sx={{ display: pathname === "/login" || pathname === "/signup" ? "none" : "" }}
+        <AppBar position="static" color="divider">
+            <Toolbar>
+                <Typography 
+                    variant="h6" 
+                    component={Link} to="/" 
+                    sx={{ 
+                        flexGrow: 1, 
+                        textAlign: "left", 
+                        "&:hover": { textDecoration: "none", color: "primary.main" } 
+                    }}
                 >
-                    Login
-                </Button>
-            )}
-        </div>
+                    Header
+                </Typography>
+
+                {false ? (
+                    <>
+                        <CartButton />
+
+                        <IconButton>
+                            <Avatar 
+                                alt="username"
+                                src=""
+                                sx={{ width: 24, height: 24 }} 
+                            />
+                        </IconButton>
+                    </>
+                ) : (
+                    <Button 
+                        component={Link} to="/login" 
+                        // variant="contained"
+                        sx={{ display: pathname === "/login" || pathname === "/signup" ? "none" : "" }}
+                    >
+                        Login
+                    </Button>
+                )}
+            </Toolbar>
+        </AppBar>
     )
 }
