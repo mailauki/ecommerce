bf123ad077a2f722030beb9fbad2594fd27e40af
Julie Evans <juliemevans17@gmail.com>
2023-03-19 00:52:01 -0500

    Update image grid on Home page

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,20 +1,45 @@
-import "../styles/Card.css";
-import { Card, CardMedia, CardContent, CardActionArea } from "@mui/material";
+import { Card, CardMedia, CardContent, CardActionArea, ImageListItem, ImageListItemBar } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export default function ProductCard({ product, index }) {
+    const image = product.images ? product.images[0].url : `https://dummyimage.com/640x640/ccc/555/&text=${product.name || product.title}`
+
     return (
-        <Card className={`Card ${index === 0 ? "first" : ""}`} sx={{ borderRadius: 0 }}>
-            <CardActionArea component={Link} to={`/products/${product.id || product._id}`}>
-                <CardMedia 
-                    component="img"
-                    src={product.images ? product.images[0].url : `https://dummyimage.com/640x640/ccc/555/&text=${product.name || product.title}`}
-                    title="image"
-                />
-                <CardContent sx={{ height: "90px" }}>
-                    <p>{product.name || product.title}</p>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        // <Card className={`Card ${index === 0 ? "first" : ""}`} sx={{ borderRadius: 0 }}>
+        //     <CardActionArea component={Link} to={`/products/${product.id || product._id}`}>
+        //         <CardMedia 
+        //             component="img"
+        //             src={product.images ? product.images[0].url : `https://dummyimage.com/640x640/ccc/555/&text=${product.name || product.title}`}
+        //             title="image"
+        //         />
+        //         <CardContent sx={{ height: "90px" }}>
+        //             <p>{product.name || product.title}</p>
+        //         </CardContent>
+        //     </CardActionArea>
+        // </Card>
+        <ImageListItem 
+            rows={index === 0 ? 2 : 1} 
+            sx={{ 
+                "&:hover": { ".product-info": { display: "flex" } }
+            }} 
+            component={Link} to={`/products/${product.id || product._id}`}
+        >
+            <img 
+                src={image}
+                alt={product.name || product.title}
+                loading="lazy"
+            />
+            <ImageListItemBar
+                className="product-info"
+                sx={{
+                    background:
+                        "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+                        "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
+                    display: "none"
+                }}
+                title={product.name || product.title}
+                position="top"
+            />
+        </ImageListItem>
     )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import "../styles/Products.css";
 import ProductCard from "../components/ProductCard";
+import { ImageList } from "@mui/material";
 
 export default function Home() {
     const [products, setProducts] = useState(null);
 
     useEffect(() => {
-        fetch('https://api.storerestapi.com/products/')
+        fetch('https://api.storerestapi.com/products?limit=8')
         .then((r) => r.json())
         .then((data) => setProducts(data.data))
     }, [])
@@ -18,15 +19,26 @@ export default function Home() {
     
     return (
         <>
-            <div className="grid">
-                {gridProducts.map((product) => <ProductCard key={product._id} product={product} />)}
-            </div>
+            <ImageList
+                sx={{ width: "100%" }}
+                variant="quilted" 
+                cols={2}
+                rowHeight={200}
+            >
+                {gridProducts.map((product, index) => (
+                    <ProductCard key={product.id || product._id} product={product} index={index} />
+                ) )}
+            </ImageList>
 
-            <div style={{ width: "100vw", overflow: "hidden" }}>
+            {/* <div className="grid">
+                {gridProducts.map((product) => <ProductCard key={product._id} product={product} />)}
+            </div> */}
+
+            {/* <div style={{ width: "100vw", overflow: "hidden" }}>
                 <div className="slider">
                     {sliderProducts.map((product, index) => <ProductCard key={product._id} product={product} index={index} />)}
                 </div>
-            </div>
+            </div> */}
         </>
     )
 }
